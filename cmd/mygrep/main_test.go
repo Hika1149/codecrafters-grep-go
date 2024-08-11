739ee43aabffb33e4ee180addce41b1ae6fb6b7a
Hika1149 <hikari94854@gmail.com>
2024-08-11 22:51:37 +0800

    [+]: unit test for test matcher

---
+++ cmd/mygrep/main_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/codecrafters-io/grep-starter-go/cmd/mygrep/internal"
+	"testing"
+)
+
+func TestMatcher(t *testing.T) {
+	type MatchTest struct {
+		line     []byte
+		pattern  string
+		expected bool
+	}
+
+	var matchLineTests = []MatchTest{
+
+		{[]byte("dog"), "d", true},
+	}
+
+	for _, tt := range matchLineTests {
+		m := internal.NewMatcher().ScanPattern(tt.pattern).Match(tt.line)
+
+		if m != tt.expected {
+			t.Errorf("line=%v pattern=%v Expected %v, but got %v\n", string(tt.line), tt.pattern, tt.expected, m)
+		}
+	}
+
+}
